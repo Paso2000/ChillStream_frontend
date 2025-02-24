38e3050b67ac91ea2e976889156513baa23e48f5
Luca Pasini <luca.pasini8@studio.unibo.it>
2025-02-24 10:34:48 +0100

    adding content and interaction API

--- src/service/contentApi.js
+++ src/service/contentApi.js
@@ -1,9 +1,117 @@
 import axios from "axios";
 
-// Creiamo un'istanza di Axios con il base URL del backend
 const contentApi = axios.create({
     baseURL: "http://localhost:8080/",
 });
+export const getFilmList = async () => {
+    try {
+        const response = await contentApi.get("/films/");
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting film:", error);
+        throw error;
+    }
+};
+
+export const postFilm = async (body) => {
+    try {
+        const response = await contentApi.post("/films/",body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore creating film:", error);
+        throw error;
+    }
+};
+
+export const getFilm = async (filmId) => {
+    try {
+        const response = await contentApi.get(`/films/${filmId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting the film:", error);
+        throw error;
+    }
+};
+
+export const putFilm = async (filmId, body) => {
+    try {
+        const response = await contentApi.put(`/films/${filmId}`, body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore changing the film:", error);
+        throw error;
+    }
+};
+
+export const deleteFilm = async (filmId) => {
+    try {
+        const response = await contentApi.delete(`/films/${filmId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore deleting the film:", error);
+        throw error;
+    }
+};
+
+export const getActorList = async () => {
+    try {
+        const response = await contentApi.get("/actors/");
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting actors:", error);
+        throw error;
+    }
+};
+
+export const postActors = async (body) => {
+    try {
+        const response = await contentApi.post("/actors/",body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore creating actor:", error);
+        throw error;
+    }
+};
+
+export const getActor = async (actorId) => {
+    try {
+        const response = await contentApi.get(`/actors/${actorId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting the actor:", error);
+        throw error;
+    }
+};
+
+export const putActor = async (actorId, body) => {
+    try {
+        const response = await contentApi.put(`/actors/${actorId}`, body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore changing the actor:", error);
+        throw error;
+    }
+};
+
+export const deleteActor = async (actorId) => {
+    try {
+        const response = await contentApi.delete(`/actors/${actorId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore deleting the actor:", error);
+        throw error;
+    }
+};
+
+export const getFilmByActorId = async (actorId) => {
+    try {
+        const response = await contentApi.get(`/actors/${actorId}/films`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore deleting the actor:", error);
+        throw error;
+    }
+};
 
 
 export default contentApi;
---
+++ src/service/interactionApi.js
@@ -0,0 +1,168 @@
+import axios from "axios";
+
+const interactionApi = axios.create({
+    baseURL: "http://localhost:8082/users/",
+});
+export const getViewList = async (userId, profileId) => {
+    try {
+        const response = await interactionApi.get(`/${userId}/profiles/${profileId}/notifications`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting view:", error);
+        throw error;
+    }
+};
+
+export const postView = async (userId, profileId,body) => {
+    try {
+        const response = await interactionApi.post(`/${userId}/profiles/${profileId}/notifications`,body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore creating film:", error);
+        throw error;
+    }
+};
+
+export const getView = async (userId, profileId, viewId) => {
+    try {
+        const response = await interactionApi.get(`/${userId}/profiles/${profileId}/notifications/${viewId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting the view:", error);
+        throw error;
+    }
+};
+
+export const putView = async (userId, profileId, viewId, body) => {
+    try {
+        const response = await interactionApi.put(`/${userId}/profiles/${profileId}/notifications/${viewId}`, body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore changing the view:", error);
+        throw error;
+    }
+};
+
+export const deleteView = async (userId, profileId, viewId) => {
+    try {
+        const response = await interactionApi.delete(`/${userId}/profiles/${profileId}/notifications/${viewId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore deleting the view:", error);
+        throw error;
+    }
+};
+
+
+export const getRecommendedList = async (userId, profileId) => {
+    try {
+        const response = await interactionApi.get(`/${userId}/profiles/${profileId}/notifications`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting Recommended :", error);
+        throw error;
+    }
+};
+
+export const postRecommended = async (userId, profileId,body) => {
+    try {
+        const response = await interactionApi.post(`/${userId}/profiles/${profileId}/notifications`,body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore creating Recommended:", error);
+        throw error;
+    }
+};
+
+export const getRecommended = async (userId, profileId, recommendedId ) => {
+    try {
+        const response = await interactionApi.get(`/${userId}/profiles/${profileId}/notifications/${recommendedId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting the recommended:", error);
+        throw error;
+    }
+};
+
+export const putRecommended = async (userId, profileId, recommendedId, body) => {
+    try {
+        const response = await interactionApi.put(`/${userId}/profiles/${profileId}/notifications/${recommendedId}`, body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore changing the recommended:", error);
+        throw error;
+    }
+};
+
+export const deleteRecommended = async (userId, profileId, recommendedId) => {
+    try {
+        const response = await interactionApi.delete(`/${userId}/profiles/${profileId}/notifications/${recommendedId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore deleting the recommended:", error);
+        throw error;
+    }
+};
+
+export const getReactionList = async (userId, profileId) => {
+    try {
+        const response = await interactionApi.get(`/${userId}/profiles/${profileId}/notifications`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting reactions :", error);
+        throw error;
+    }
+};
+
+export const postReaction = async (userId, profileId,body) => {
+    try {
+        const response = await interactionApi.post(`/${userId}/profiles/${profileId}/notifications`,body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore creating reactions:", error);
+        throw error;
+    }
+};
+
+
+export const deleteReaction = async (userId, profileId, reactionsId) => {
+    try {
+        const response = await interactionApi.delete(`/${userId}/profiles/${profileId}/notifications/${reactionsId}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore deleting the reactions:", error);
+        throw error;
+    }
+};
+
+export const getNotificationList = async (userId, profileId) => {
+    try {
+        const response = await interactionApi.get(`/${userId}/profiles/${profileId}/notifications`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore getting notifications :", error);
+        throw error;
+    }
+};
+
+export const postNotification = async (userId, profileId,body) => {
+    try {
+        const response = await interactionApi.post(`/${userId}/profiles/${profileId}/notifications`,body);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore creating notifications:", error);
+        throw error;
+    }
+};
+
+
+export const deleteNotification = async (userId, profileId, notificationsID) => {
+    try {
+        const response = await interactionApi.delete(`/${userId}/profiles/${profileId}/notifications/${notificationsID}`);
+        return response.data; // Ritorna il token o i dati dell'utente
+    } catch (error) {
+        console.error("Errore deleting the notifications:", error);
+        throw error;
+    }
+};
+
