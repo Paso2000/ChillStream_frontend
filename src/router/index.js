98bf54f26d267e267c19f03367ae84755cd93019
Luca Pasini <luca.pasini8@studio.unibo.it>
2025-02-25 13:09:14 +0100

    package things

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-<<<<<<< HEAD
-import LoginPage from "@/views/authentication/LoginPage.vue";
-import ProfileSelectionPage from "@/views/authentication/ProfileSelectionPage.vue";
-import RegisterPage from "@/views/authentication/RegisterPage.vue";
-import StartingPage from "@/views/authentication/StartingPage.vue";
-import createProfilePage from "@/views/authentication/CreateProfilePage.vue";
+import LoginPage from "@/views/userAuth/LoginPage.vue";
+import ProfileSelectionPage from "@/views/userAuth/ProfileSelectionPage.vue";
+import RegisterPage from "@/views/userAuth/RegisterPage.vue";
+import StartingPage from "@/views/userAuth/StartingPage.vue";
+import createProfilePage from "@/views/userAuth/CreateProfilePage.vue";
 import HomePage from "@/views/HomePage.vue";
 import AdminLoginPage from "@/views/admin/AdminLoginPage.vue";
 import ControlPanelPage from "@/views/admin/controlPanelPage.vue";
