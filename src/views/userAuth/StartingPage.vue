f4193a2694453f7e75f6aa61b8d9845e26c38f13
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2025-05-13 23:45:52 +0200

    recat style about first page

--- src/views/userAuth/StartingPage.vue
+++ src/views/userAuth/StartingPage.vue
@@ -76,11 +76,12 @@ const goToRegister = () => {
 @media (min-width: 769px) {
   .navbar {
     justify-content: space-between;
-    padding: 0 50px;
+    margin-top: -5px;
   }
 
   .logo-container {
     display: block;
+
   }
 
   .sign-in-btn {
