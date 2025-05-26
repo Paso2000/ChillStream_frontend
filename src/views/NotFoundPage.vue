6418ef5eb11929b53ec007ddde12b9fb61be3e5b
Luca Pasini <luca.pasini8@studio.unibo.it>
2025-05-26 14:45:20 +0200

    refactor package

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="not-found">
-    <h1>404 - Page Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
+    <BackButton />
+    <div class="content">
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
+    </div>
   </div>
 </template>
 
 <script setup>
+import BackButton from "@/components/BackButton.vue";
 </script>
 
 <style scoped>
 .not-found {
-  text-align: center;
-  padding: 50px;
+  padding: 20px;
   color: white;
+  background-color: black;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.content {
+  margin: auto;
+  text-align: center;
+  width: 100%;
 }
 </style>
+
